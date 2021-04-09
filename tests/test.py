14f91adef9331c417c047031f49270d705915a05
Rajasri-kolli <54495276+Rajasri-kolli@users.noreply.github.com>
2021-04-09 21:32:29 +0530

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,83 @@
 
+import redactor
+import nltk
+
+files =  ['*.txt']
+
+
+def test_read_input():
+    files_data = (redactor.read_input(files))
+    assert type(redactor.read_input(files)) == list
+    for i in range(len(files_data)):
+        temp_file = files_data[i]
+        assert (len(temp_file)) > 10
+    return 0
+
+
+def test_redact_names():
+    counter = 0
+    list = (redactor.Reading_input(files))
+    redacted_list = redactor.redact_names(list)
+    # print(len(redacted_list))
+    for i in range(len(redacted_list)):
+        temp_file = redacted_list[i]
+        words = nltk.word_tokenize(temp_file)
+        for j in words:
+            # print (j)
+            if '\u2588' in j:
+                counter += 1
+    assert counter is not None
+
+def test_get_gender():
+    counter =0
+    list = (redactor.read_input(files))
+    redacted_list = redactor.get_gender(list)
+    # print(len(redacted_list))
+    for i in range(len(redacted_list)):
+        temp_file = redacted_list[i]
+        words = nltk.word_tokenize(temp_file)
+        for j in words:
+            if '^' in j:
+                counter += 1
+    assert counter is not None
+
+def test_get_date():
+    counter =0
+    list = (redactor.read_input(files))
+    redacted_list = redactor.get_date(list)
+    for i in range(len(redacted_list)):
+        temp_file = redacted_list[i]
+        words = nltk.word_tokenize(temp_file)
+        for j in words:
+            if '^' in j:
+                counter += 1
+    assert counter is not None
+
+
+def test_get_concept():
+    word ='sharp'
+    counter =0
+    list = (redactor.read_input(files))
+    redacted_list = redactor.get_concept(list,word)
+    print((redacted_list))
+    for i in range(len(redacted_list)):
+        temp_file = redacted_list[i]
+        words = nltk.word_tokenize(temp_file)
+        for j in words:
+            if '^' in j:
+                counter += 1
+    assert counter is not None
+
+
+def test_get_stats():
+    a = redactor.read_input(files)
+    a = redactor.get_names(a)
+    a = redactor.get_gender(a)
+    a= redactor.get_date(a)
+    word = 'sharp'
+    redactor.get_concept(a,word)
+    stats_list =redactor.stats_list
+    assert stats_list is not None
+
+
+
